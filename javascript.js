740c80304c111ffcab0b08c854b279f00bc47871
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-06 10:13:22 +0530

    Update game UI
    Though the game is almost finished, there are some functional problems,
    needed to be fixed.
    
    The player's and computer's scores are not updating in the display.
    Need to update final result in the display.

--- javascript.js
+++ javascript.js
@@ -1,44 +1,14 @@
-//Pseudocode for the program.
 
-//A Function that gets a random output with the choices of "Rock , Paper and Scissors"
-// to act as the computer's side.
-  //no parameter required.
-  //store these three strings in an array
-  //select one randomly from the array
-  //return it
-//Another function for the logic of the game
-//Two parameters - one from the computer's returned value and  
-//another from the user's input, through the prompt
-//compare these two
-    //if two values are same, print(It's a tie)
-    //a block of code that checks who wins according to the logic of the game
-    // return the statement
-//Another function to make the game run for five times iteratively
-    //have to pass the previous function as a parameter
-    //need to have a counter to store the scores of the players
-    //a for loop to serve the purpose
-    // and finally to display the scores and the winner in the console
-
- function getComputerChoice(){
+function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * choices.length) ;
     return choices[choice];
 }  
 
-let input;
-let computerSelection;
-
 function checkForWinner(playerChoice, computerChoice){
-    let validInput = false;
-    while(!validInput){
-        input = prompt("Enter Your Choice. rock , paper or scissors (Enter a valid input): ");
-        if (input == "rock" || input == "paper" || input == "scissors"){
-            validInput = true;
-        }
-    }
-    playerChoice = input.toLowerCase();
     computerSelection = getComputerChoice();
     computerChoice = computerSelection;
+    playerChoice = playerSelection;
     if (playerChoice == "rock" && computerChoice == "rock"){
         return "It's a tie";
     } else if  (playerChoice == "rock" && computerChoice == "scissors"){
@@ -61,26 +31,39 @@ function checkForWinner(playerChoice, computerChoice){
     
 }
 
-function game(){
+function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    for (i = 0; i < 5; i++){
-        let result = checkForWinner();
-        console.log(result);
-        console.log("Player's Choice : " + input, ", Computer's Choice: " , computerSelection);
-        let positiveRes = new RegExp("You won!*");
-        let negativeRes = new RegExp("You lose!*");
-        if (positiveRes.test(result)){
-            playerScore = ++playerScore; 
-        } else if (negativeRes.test(result)){
-            computerScore = ++computerScore;
-        } 
-        console.log("Player's Score: " + playerScore, ", Computer's Score: " + computerScore);
-        console.log(" ");
-    }
-    let finalResult = playerScore > computerScore ? " Congratulations!!! You Won the Computer.":
+    let result = checkForWinner();
+    const score = document.querySelector(".para1")
+    score.textContent = result;
+    let positiveRes = new RegExp("You won!*");
+    let negativeRes = new RegExp("You lose!*");
+    if (positiveRes.test(result)){
+        playerScore = ++playerScore; 
+    } else if (negativeRes.test(result)){
+        computerScore = ++computerScore;
+    } 
+    const scoreP = document.querySelector(".score_p");
+    scoreP.textContent = "Player's score: " + playerScore;
+    const scoreC = document.querySelector(".score_c");
+    scoreC.textContent = "Computer's Score: " + computerScore;
+    const playerRes = document.querySelector(".player_choice");
+    playerRes.textContent = "Player's Choice : " + playerSelection;
+    const computerRes  = document.querySelector(".computer_choice");
+    computerRes.textContent = "Computer's Choice: " + computerSelection;
+    let finalResult = playerScore > computerScore ? " Congratulations! You Won the Computer.":
     computerScore > playerScore ? "Sorry! Computer won the game.":
     "It's a tie. No one wins.";
-    console.log(finalResult);
+    const scoreRes = document.querySelector(".para2");
+    scoreRes.textContent = finalResult;
+    
 }
-game();
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) =>{
+    button.addEventListener('click', action => {
+        playerSelection = button.id;
+        playGame();
+    })
+});
+
